901b0e8511e6355fa3aff1d7ee1a43673fb9c686
denstav <agressclash@gmail.com>
2022-03-20 13:13:56 +0300

    !

--- js/signup.js
+++ js/signup.js
@@ -1,7 +1,52 @@
+let form = document.getElementById('form'),
+    formInputs = document.querySelectorAll('form__input'),
+    inputPhone = document.querySelector('form__phone'),
+    inputCheckbox = document.querySelector('form__checkbox');
+
+
+function validateCountry(country) {
+    let re = new RegExp('.co$');
+    return re.test(String(country).toLowerCase());
+}
+
+function validatePhone(phone) {
+    let re = /^[0-9\s]*$/;
+    return re.test(String(phone));
+}
+
+form.onsubmit = function () {
+        phoneVal = inputPhone.value,
+        emptyInputs = Array.from(formInputs).filter(input => input.value === '');
+
+    formInputs.forEach(function (input) {
+        if (input.value === '') {
+            input.classList.add('error');
+
+        } else {
+            input.classList.remove('error');
+        }
+    });
+
+    if (emptyInputs.length !== 0) {
+        console.log('inputs not filled');
+        return false;
     }
-  }
+
+    if (!validatePhone(phoneVal)) {
+        console.log('phone not valid');
+        inputPhone.classList.add('error');
+        return false;
+    } else {
+        inputPhone.classList.remove('error');
+    }
+
+    if(!inputCheckbox.checked) {
+        console.log('checkbox not checked');
+        inputCheckbox.classList.add('error');
+        return false;
+    } else {
+        inputCheckbox.classList.remove('error')
+    }
 
 
-
-
-})
+}
