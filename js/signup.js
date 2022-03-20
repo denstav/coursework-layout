76ed50e0b2997d87298381eeadf91aa489e8ffbe
denstav <agressclash@gmail.com>
2022-03-20 13:13:27 +0300

    !

--- js/signup.js
+++ js/signup.js
@@ -1,52 +1,7 @@
-'use strict'
-
-document.addEventListener('DOMContentLoaded',function(){
-
-  const form = document.getElementById('form');
-  const emailError = document.querySelector('text__error');
-  console.log(form)
-  form.addEventListener('submit',formSend);
-
-  async function formSend(e){
-    e.preventDefault();
-
-    let error = formValidate(form)
-  }
-
-  function formValidate(form){
-    let error = 0
-    let formReq = document.querySelectorAll('._req');
-
-    for (let index = 0; index < formReq.length; index++) {
-      const input = formReq[index];
-      formRemoveError(input)
-      
-      if(input.value === ''){
-        formAddError(input);
-        error++;
-      
-  
-      }
-        if(input.validity.valueMissing) {
-          // Если поле пустое,
-          // отображаем следующее сообщение об ошибке
-          emailError.textContent = 'You need to enter an e-mail address.';
-      }
     }
   }
 
-  function formAddError(input){
-    input.parentElement.classList.add('_error');
-    input.classList.add('_error');
-    
-  }
 
-  function formRemoveError(input){
-    input.parentElement.classList.remove('_error');
-    input.classList.remove('_error');
-    
-  }
-  
 
 
 })
