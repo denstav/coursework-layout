6ceb5dbcff3300c1c187dd38728a99f9e35155ab
denstav <agressclash@gmail.com>
2022-03-21 01:21:32 +0300

    !

--- js/signup.js
+++ js/signup.js
@@ -1,50 +1,72 @@
-'use strict'
+const form = document.getElementById('form');
+const username = document.getElementById('username');
+const email = document.getElementById('email');
+const password = document.getElementById('password');
+const password2 = document.getElementById('password2');
 
-document.addEventListener('DOMContentLoaded',function(){
+form.addEventListener('submit', e => {
+    e.preventDefault();
 
-    const form = document.getElementById('form');
-    let textError = document.querySelector('text__error')
-    form.addEventListener('submit',formSend);
+    validateInputs();
+});
 
-    async function formSend(e){
-        e.preventDefault();
-        for (const elem of form.elements) {
+const setError = (element, message) => {
+    const inputControl = element.parentElement;
+    const errorDisplay = inputControl.querySelector('.error');
 
-            if(elem.value === ''){
-                elem.nextElementSibling
-            }
-            
-        }
-        
+    errorDisplay.innerText = message;
+    inputControl.classList.add('error');
+    inputControl.classList.remove('success')
+}
 
-        let error = formValidate(form)
+const setSuccess = element => {
+    const inputControl = element.parentElement;
+    const errorDisplay = inputControl.querySelector('.error');
+
+    errorDisplay.innerText = '';
+    inputControl.classList.add('success');
+    inputControl.classList.remove('error');
+};
+
+const isValidEmail = email => {
+    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+}
+
+const validateInputs = () => {
+    const usernameValue = username.value.trim();
+    const emailValue = email.value.trim();
+    const passwordValue = password.value.trim();
+    const password2Value = password2.value.trim();
+
+    if(usernameValue === '') {
+        setError(username, 'Введи корректное имя');
+    } else {
+        setSuccess(username);
     }
-    
 
-    function formValidate(form){
-        let error = 0
-        let formReq = document.querySelectorAll('._req');
-    
-        for (let index = 0; index < formReq.length; index++) {
-          const input = formReq[index];
-          formRemoveError(input);
-          
-          if(input.value === ''){
-            formAddError(input);
-            error++;
-          }
-        }
-      }
-      
-      function formAddError(input){
-        input.parentElement.classList.add('_error');
-        input.classList.add('_error');
-        
-      }
+    if(emailValue === '') {
+        setError(email, 'Это поле обязательное');
+    } else if (!isValidEmail(emailValue)) {
+        setError(email, 'Пожалуйста, введи корректный e-mail');
+    } else {
+        setSuccess(email);
+    }
 
-      function formRemoveError(input){
-        input.parentElement.classList.remove('_error');
-        input.classList.remove('_error');
-        
-      }
-})
+    if(passwordValue === '') {
+        setError(password, 'Пожалуйста, введи корректный пароль');
+    } else if (passwordValue.length < 8 ) {
+        setError(password, 'Пароль должен быть не менее 8 символов.')
+    } else {
+        setSuccess(password);
+    }
+
+    if(password2Value === '') {
+        setError(password2, 'Пожалуйста, подтвердите свой пароль');
+    } else if (password2Value !== passwordValue) {
+        setError(password2, "Пароли не совпадают");
+    } else {
+        setSuccess(password2);
+    }
+
+};
