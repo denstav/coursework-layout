f900eaaa2a890075391a76a52b37468d68dc08cb
denstav <agressclash@gmail.com>
2022-03-27 15:08:19 +0300

    !

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-var slideIndex = 1;
+let slideIndex = 1;
 showSlides(slideIndex);
 
 function plusSlides(n) {
@@ -10,11 +10,16 @@ function currentSlide(n) {
 }
 
 function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}    
-  if (n < 1) {slideIndex = slides.length}
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+
+  if (n > slides.length){
+    slideIndex = 1
+  }    
+  if (n < 1){
+    slideIndex = slides.length
+  }
   for (i = 0; i < slides.length; i++) {
       slides[i].style.display = "none";  
   }
@@ -24,7 +29,9 @@ function showSlides(n) {
   slides[slideIndex-1].style.display = "block";  
   dots[slideIndex-1].className += " active";
   slideIndex++;
-  if (slideIndex > slides.length) {slideIndex = 1}
+  if (slideIndex > slides.length){
+    slideIndex = 1
+  }
   slides[slideIndex-1].style.display = "none";
   setTimeout(showSlides, 5000);
 }
