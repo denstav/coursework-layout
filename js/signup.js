779497a0f4e8a81ff0b7cdb394cbae8f25e5de34
denstav <agressclash@gmail.com>
2022-04-03 20:18:45 +0300

    !

--- js/signup.js
+++ js/signup.js
@@ -1,4 +1,3 @@
-
 // Модальное окно //
 
 let signupBg = document.querySelector('.signup__bg')
@@ -6,13 +5,13 @@ let signupModal = document.querySelector('.signup__moda')
 let openSignup = document.querySelector('.btn__signup')
 let closeSignup = document.querySelector('.signup__navigation-close')
 
-openSignup.addEventListener('click',function(event){
+openSignup.addEventListener('click', function (event) {
     event.preventDefault()
     signupBg.classList.add('active')
     signupModal.classList.add('active')
 
 })
-closeSignup.addEventListener('click',function(){
+closeSignup.addEventListener('click', function () {
     signupBg.classList.remove('active')
     signupModal.classList.remove('active')
 })
@@ -24,85 +23,86 @@ form.addEventListener('submit', e => {
     let username = document.getElementById('username');
     let email = document.getElementById('email');
     let password = document.getElementById('password');
-    
-    function validation(){
+
+    let isError = false
+
+
+    function validation() {
         const setError = (element, message) => {
             const inputControl = element.parentElement;
             const errorDisplay = inputControl.querySelector('.error');
-        
+
             errorDisplay.innerText = message;
             inputControl.classList.add('error');
             inputControl.classList.remove('success')
         }
-        
+
         const setSuccess = element => {
             const inputControl = element.parentElement;
             const errorDisplay = inputControl.querySelector('.error');
-        
+
             errorDisplay.innerText = '';
             inputControl.classList.add('success');
             inputControl.classList.remove('error');
         };
-        
+
         const isValidEmail = email => {
             const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
             return re.test(String(email).toLowerCase());
         }
-        
-            const usernameValue = username.value.trim();
-            const emailValue = email.value.trim();
-            const passwordValue = password.value.trim();
-        
-            if(usernameValue === '') {
-                setError(username, 'Это обязательное поле');
-            } else {
-                setSuccess(username);
-                
-            }
-        
-            if(emailValue === '') {
-                setError(email, 'Это поле обязательное');
-            } else if (!isValidEmail(emailValue)) {
-                setError(email, 'Пожалуйста, введи корректный e-mail');
-            } else {
-                setSuccess(email);
-                
-            }
-        
-            if(passwordValue === '') {
-                setError(password, 'Пожалуйста, введи корректный пароль');
-            } else if (passwordValue.length < 8 ) {
-                setError(password, 'Пароль должен быть не менее 8 символов.')
-            } else {
-                setSuccess(password);
-                
-            }
+
+        const usernameValue = username.value;
+        const emailValue = email.value;
+        const passwordValue = password.value;
+
+        if (usernameValue === '') {
+            setError(username, 'Это обязательное поле');
+            isError = true
+        } else {
+            setSuccess(username);
+
+        }
+
+        if (emailValue === '') {
+            setError(email, 'Это поле обязательное');
+            isError = true
+        } else if (!isValidEmail(emailValue)) {
+            setError(email, 'Пожалуйста, введи корректный e-mail');
+            isError = true
+        } else {
+            setSuccess(email);
+
+        }
+
+        if (passwordValue === '') {
+            setError(password, 'Пожалуйста, введи корректный пароль');
+            isError = true
+        } else if (passwordValue.length < 8) {
+            setError(password, 'Пароль должен быть не менее 8 символов.')
+            isError = true
+        } else {
+            setSuccess(password);
+
+        }
+
     }
-    
+
     validation()
-    
-        
+    let uid = new Date().getTime()
+
+    if (!isError){
         let user = {
-            name: username,
-            email: email,
-            password: password,
+            name: username.value,
+            email: email.value,
+            password: password.value,
         }
         let json = JSON.stringify(user)
-        localStorage.setItem('user',json)
-        
+        localStorage.setItem(`user${uid}`, json)
+        window.location = '/privateOffice.html'
+
+    }
+
+    
+
 
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
