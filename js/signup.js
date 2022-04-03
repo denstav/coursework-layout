48ac0a6d4a7b496cf873b7296d66aaddff80a48a
denstav <agressclash@gmail.com>
2022-04-03 18:21:22 +0300

    !

--- js/signup.js
+++ js/signup.js
@@ -17,26 +17,80 @@ closeSignup.addEventListener('click',function(){
     signupModal.classList.remove('active')
 })
 
-//Регистрация
-
-
-
-
-
 const form = document.getElementById('form');
-const username = document.getElementById('username');
-const email = document.getElementById('email');
-const password = document.getElementById('password');
 
-let btnSignup = document.querySelector('.btn__form-signup')
-
-btnSignup.addEventListener('click', e => {
-    e.preventDefault();
-
-    validateInputs();
-
+form.addEventListener('submit', e => {
+    e.preventDefault()
+    let username = document.getElementById('username');
+    let email = document.getElementById('email');
+    let password = document.getElementById('password');
     
+    function validation(){
+        const setError = (element, message) => {
+            const inputControl = element.parentElement;
+            const errorDisplay = inputControl.querySelector('.error');
+        
+            errorDisplay.innerText = message;
+            inputControl.classList.add('error');
+            inputControl.classList.remove('success')
+        }
+        
+        const setSuccess = element => {
+            const inputControl = element.parentElement;
+            const errorDisplay = inputControl.querySelector('.error');
+        
+            errorDisplay.innerText = '';
+            inputControl.classList.add('success');
+            inputControl.classList.remove('error');
+        };
+        
+        const isValidEmail = email => {
+            const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+            return re.test(String(email).toLowerCase());
+        }
+        
+            const usernameValue = username.value.trim();
+            const emailValue = email.value.trim();
+            const passwordValue = password.value.trim();
+        
+            if(usernameValue === '') {
+                setError(username, 'Это обязательное поле');
+            } else {
+                setSuccess(username);
+                
+            }
+        
+            if(emailValue === '') {
+                setError(email, 'Это поле обязательное');
+            } else if (!isValidEmail(emailValue)) {
+                setError(email, 'Пожалуйста, введи корректный e-mail');
+            } else {
+                setSuccess(email);
+                
+            }
+        
+            if(passwordValue === '') {
+                setError(password, 'Пожалуйста, введи корректный пароль');
+            } else if (passwordValue.length < 8 ) {
+                setError(password, 'Пароль должен быть не менее 8 символов.')
+            } else {
+                setSuccess(password);
+                
+            }
+    }
     
+    validation()
+    
+        
+        let user = {
+            name: username,
+            email: email,
+            password: password,
+        }
+        let json = JSON.stringify(user)
+        localStorage.setItem('user',json)
+        
+
 });
 
 
@@ -45,65 +99,10 @@ btnSignup.addEventListener('click', e => {
 
 
 
-const setError = (element, message) => {
-    const inputControl = element.parentElement;
-    const errorDisplay = inputControl.querySelector('.error');
 
-    errorDisplay.innerText = message;
-    inputControl.classList.add('error');
-    inputControl.classList.remove('success')
-}
 
-const setSuccess = element => {
-    const inputControl = element.parentElement;
-    const errorDisplay = inputControl.querySelector('.error');
 
-    errorDisplay.innerText = '';
-    inputControl.classList.add('success');
-    inputControl.classList.remove('error');
-};
 
-const isValidEmail = email => {
-    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return re.test(String(email).toLowerCase());
-}
 
-const validateInputs = () => {
-    const usernameValue = username.value.trim();
-    const emailValue = email.value.trim();
-    const passwordValue = password.value.trim();
-
-    if(usernameValue === '') {
-        setError(username, 'Это обязательное поле');
-    } else {
-        setSuccess(username);
-    }
-
-    if(emailValue === '') {
-        setError(email, 'Это поле обязательное');
-    } else if (!isValidEmail(emailValue)) {
-        setError(email, 'Пожалуйста, введи корректный e-mail');
-    } else {
-        setSuccess(email);
-    }
-
-    if(passwordValue === '') {
-        setError(password, 'Пожалуйста, введи корректный пароль');
-    } else if (passwordValue.length < 8 ) {
-        setError(password, 'Пароль должен быть не менее 8 символов.')
-    } else {
-        setSuccess(password);
-    }
-
-   
-};
-
-let user = {
-    name: username,
-    email: email,
-    password: password,
-}
-let json = JSON.stringify(user)
-localStorage.setItem('user',json)
 
 
