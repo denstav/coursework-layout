312b16cefe097678a3e301bd31c5ad9dba0867ec
denstav <agressclash@gmail.com>
2022-04-15 18:01:37 +0300

    !

--- js/office.js
+++ js/office.js
@@ -2,11 +2,15 @@ let text = document.querySelector('.main__content-right__text')
 let textProfile = document.querySelectorAll('.main__content-left__text')
 let textOffice = document.querySelector('.main__content-right__text-one')
 let userName = document.querySelector('.main__content-right__user');
-
+let textConsol = document.querySelectorAll('.main__content-right')
 userName.textContent = `${JSON.parse(localStorage.getItem('currUser'))['name']}`
+
+
+
 
 textProfile[1].addEventListener('click',function(){
     text.innerHTML = `Ваша почта: ${JSON.parse(localStorage.getItem('currUser'))['email']}`;
+   
 
 })
 
