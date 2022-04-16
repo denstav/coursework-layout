ea5831113faf3b0354693616aebae840c0fb022d
denstav <agressclash@gmail.com>
2022-04-16 19:12:30 +0300

    !

--- js/office.js
+++ js/office.js
@@ -1,5 +1,5 @@
 let text = document.querySelector('.main__content-right__text')
-let textProfile = document.querySelectorAll('.main__content-left__text')
+let textProfile = document.querySelectorAll('.content__tabs')[1]
 let textOffice = document.querySelector('.main__content-right__text-one')
 let userName = document.querySelector('.main__content-right__user');
 let textConsol = document.querySelectorAll('.main__content-right')
--- js/slider.js
+++ js/slider.js
@@ -14,24 +14,24 @@ function showSlides(n) {
   let slides = document.getElementsByClassName("mySlides");
   let dots = document.getElementsByClassName("dot");
 
-  if (n > slides.length){
+  if (n > slides.length) {
     slideIndex = 1
-  }    
-  if (n < 1){
+  }
+  if (n < 1) {
     slideIndex = slides.length
   }
   for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";  
+    slides[i].style.display = "none";
   }
   for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
+    dots[i].className = dots[i].className.replace(" active", "");
   }
-  slides[slideIndex-1].style.display = "block";  
-  dots[slideIndex-1].className += " active";
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
   slideIndex++;
-  if (slideIndex > slides.length){
+  if (slideIndex > slides.length) {
     slideIndex = 1
   }
-  slides[slideIndex-1].style.display = "none";
+  slides[slideIndex - 1].style.display = "none";
   setTimeout(showSlides, 5000);
 }
