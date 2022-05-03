a055cf9b6c255b2e18819c99c8b15800f1bbfbc6
denstav <agressclash@gmail.com>
2022-05-04 00:18:40 +0300

    !

--- js/signin.js
+++ js/signin.js
@@ -3,9 +3,9 @@ let signinBg = document.querySelector('.signin__bg')
 let signinModal = document.querySelector('.signin__modal')
 let openSignin = document.querySelectorAll('.btn__signin')
 let popup__bg = document.querySelector('.popup__bg');
-//console.log(openSignin);
 let closeSignin = document.querySelector('.signin__navigation-close')
 let users = JSON.parse(localStorage.getItem('users'));
+// let isTrueLogin = false
 
 openSignin.forEach((elem) => {
     elem.addEventListener('click',function(event){
@@ -23,16 +23,48 @@ closeSignin.addEventListener('click',function(){
 })
 
 
-// function signIn(e) {
-//     let email = document.querySelector('.username_signin').value, pwd = document.querySelector('.password__signin').value;
-//     let formData = JSON.parse(localStorage.getItem('formData')) || [];
-//     let exist = formData.length && 
-//     JSON.parse(localStorage.getItem('formData')).some(data => data.email.toLowerCase() == email && data.pwd.toLowerCase() == pwd);
-//     if(!exist){
-//         alert("Incorrect login credentials");
-//     }
-//     else{
-//         location.href = "/";
-//     }
-//     e.preventDefault();
-// }
+let signinBtn = document.querySelector('.btn__form-signin')
+let isError = false
+const setError = (element, message) => {
+    const inputControl = element.parentElement;
+    const errorDisplay = inputControl.querySelector('.error');
+
+    errorDisplay.innerText = message;
+    inputControl.classList.add('error');
+    inputControl.classList.remove('success')
+}
+
+const setSuccess = element => {
+    const inputControl = element.parentElement;
+    const errorDisplay = inputControl.querySelector('.error');
+
+    errorDisplay.innerText = '';
+    inputControl.classList.add('success');
+    inputControl.classList.remove('error');
+};
+
+
+signinBtn.addEventListener('click', function(e){
+    e.preventDefault()
+    let username = document.getElementById('username_signin').value;
+    let password = document.getElementById('password__signin').value;
+    users.forEach(user =>{
+        if(username != user.name){
+            setError(username, 'Такого логина нету');
+            isError = true
+            
+        } else{
+            setSuccess(username);
+            if(password != user.password){
+                setError(username, 'Пароль не верный');
+                isError = true
+            }
+
+        }
+        if(isError == false){
+            window.location = '/privateOffice.html'
+        }
+    })
+})
+
+
